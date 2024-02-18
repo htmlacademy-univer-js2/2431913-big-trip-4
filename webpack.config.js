c0023a8a4eb706a82ec0e44867a9591e278a670b
Agamali <muhammad.agamalieff@yandex.ru>
2024-02-18 21:57:39 +0500

    Исправление ошибок

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,38 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
+const path = require( 'path' );
+const HtmlPlugin = require( 'html-webpack-plugin' );
+const CopyPlugin = require( 'copy-webpack-plugin' );
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve( __dirname, 'build' ),
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
-    new HtmlPlugin({
+    new HtmlPlugin( {
       template: 'public/index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'public',
-          globOptions: {
-            ignore: ['**/index.html'],
-          },
+    } ),
+    new CopyPlugin( {
+      patterns: [ {
+        from: 'public',
+        globOptions: {
+          ignore: [ '**/index.html' ],
         },
-      ],
-    }),
-
+      }, ],
+    } ),
   ],
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          },
+    rules: [ {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [ '@babel/preset-env' ]
         },
       },
-    ]
-  }
+    }, ],
+  },
 };
